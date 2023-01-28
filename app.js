a5ee34bd008b06247a4fef0badd6457815a59c3b
Ealdrich3 <77710782+Ealdrich3@users.noreply.github.com>
2023-01-27 18:24:10 -0600

    assignment 2.4
    code that demonstrates the experimentation of variables

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hey")
+//old way
+var life = 100 ; // lifebar for easten
+
+life = life - 10;
+
+//90
+
+life = life - 10;
+
+var name = 'easten 1'; 
+
+var checkout = false; // BOOLEAN 
+
+var box = null; 
+
+//object synbol
+
+console.log("name")
+// disable
+
+
+// new way
+const life =100;
+
+const life =1000;  
