d33460549da4eb99d45afc83ca296b3c628c0447
Ealdrich3 <77710782+Ealdrich3@users.noreply.github.com>
2023-02-01 18:44:00 -0600

    assignment 3.4
    the study and experimentation of Functions, objects, and Events

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 document.write("pleasework");
 //old way
 var life = 100 ; // lifebar for easten
@@ -24,3 +25,43 @@ console.log(name)
 const health =100;
 
 const hp =1000;  
+*/
+
+const name="Workingstudent"; //globalscope
+
+function logger() {
+    console.log("productivetime");
+    console.log("productivetime"); //function scope
+    console.log("productivetime");
+    console.log("productivetime");
+}
+
+logger();//global scope
+
+function adder(num1, num2) {
+    console.log(num1 + num2);
+}
+
+adder(5,10);
+
+function toUpper(text) {
+    const uppercased = text.toUpperCase();
+    console.log(uppercased);
+}
+
+toUpper(name);
+
+const toupper = () =>{
+    
+}
+
+const student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+  function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
